411653b68a68414dd68399040a496f75b88f1ac5
Victor Manuel Echavarria <vicme93@gmail.com>
2020-03-27 09:57:20 -0500

    ejemplo interfases graficas java

---
+++ src/clases/Suma.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author victor.echavarria
+ */
+public class Suma extends Valores{
+    
+    public double sumar(){
+    return (this.getNumero1()+ this.getNumero2());
+    }
+    
+    
+}
